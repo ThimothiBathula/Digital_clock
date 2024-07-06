f9855ff5ff6fecd1eff7f76c1e78eeeaa0c568f4
TIMOTHI BATHULA <bt3811783@gmail.com>
2024-07-06 18:20:59 +0530

    my second commit on htm and js

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
-var h=document.getElementById("hours");
-var m=document.getElementById("min")
-var s=document.getElementById("sec")
-
-
-setInterval(()=>{
-    var a=new Date()
-    hour=a.getHours();
-    minutes=a.getMinutes(); 
-    seconds=a.getSeconds();
-    if(hour<10 ||minutes<10 ||seconds<10)
-    {
-        if(hour<10){
-        h.innerHTML="0"+hour;
-        }
-        if(minutes<10)
+var hour=document.getElementById("hours");
+var minute=document.getElementById("min")
+var second=document.getElementById("sec")
+        function time()
         {
-        m.innerHTML="0"+minutes;
+             var a=new Date()
+             hours=a.getHours();
+             minutes=a.getMinutes(); 
+             seconds=a.getSeconds();
+             if(hours<10 ||minutes<10 ||seconds<10)
+                {
+                  if(hours<10)
+                  {
+                     hour.innerHTML="0"+hours;
+                  }
+                  if(minutes<10)
+                  {
+                     minute.innerHTML="0"+minutes;
+                  }
+                  if(seconds<10)
+                  {
+                     second.innerHTML="0"+seconds;
+                  }
+                }
+             else
+                {
+                    hour.innerHTML=hours;
+                    minute.innerHTML=minutes;
+                    second.innerHTML=seconds;
+                }
         }
-        if(seconds<10){
-        s.innerHTML="0"+seconds;
-        }
-    }
-    else{
-        h.innerHTML=hour;
-        m.innerHTML=minutes;
-        s.innerHTML=seconds;
-    }
-   
-    
-},1000)
-
-console.log(new date().getSeconds())
+setInterval(time,1);
